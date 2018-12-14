57985615f77f328bb91917cd425296513358c9c2
Tyler Jones <tyler.jones@cloudreach.com>
2018-12-14 11:58:34 -0500

    initial folder setup

---
+++ automation/terraform/main.tf
@@ -0,0 +1,21 @@
+# Modules needed:
+# Aqua ALB
+# Aqua Console ALB Listener
+# Aqua Console ALB Target Group
+# Aqua Console Task Definition
+# Aqua Console Service
+# Aqua Gateway NLB
+# Aqua Gateway NLB Target group
+# Aqua Gateway NLB Listener
+# Aqua Gateway Task Definition
+# Aqua Gateway Service Definition
+# Aqua Enforcer Task Definition
+# Aqua Enforcer Daemonset
+# Aqua Console Security Group
+# Aqua Gateway Security Group
+# EcsSecurityGroupIngress1
+# EcsSecurityGroupIngress2
+# EcsSecurityGroupIngress3
+# AquaEcsTaskRole
+# Potsgres RDS Database
+
