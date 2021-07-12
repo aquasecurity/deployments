168c7d2a4569a704a93e7820cbb87ccae82dab33
syedshameersarwar <syedshameersarwar@gmail.com>
2021-07-12 22:02:37 +0500

    Added Aqua VM Enforcer Loader service

--- VM-Enforcer/RPM/scripts/preuninstall.sh
+++ VM-Enforcer/RPM/scripts/preuninstall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ENFORCER_SERVICE_FILE_NAME="aqua-enforcer.service"
+ENFORCER_LOADER_SERVICE_FILENAME="aqua-loader.service"
 
 stop_service() {
     systemctl stop ${ENFORCER_SERVICE_FILE_NAME}
@@ -10,11 +11,13 @@ stop_service() {
         error_message "Unable to stop the service. please check the logs."
     fi
     systemctl disable ${ENFORCER_SERVICE_FILE_NAME}
+    systemctl stop ${ENFORCER_LOADER_SERVICE_FILENAME}
     if [ $? -eq 0 ]; then
-        echo "Info: VM Enforcer service disabled."
+        echo "Info: VM Enforcer Loader service disabled."
     else
-        error_message "Unable to disable the service. please check the logs."
+        error_message "Unable to disable the loader service. please check the logs."
     fi
+    systemctl disable ${ENFORCER_LOADER_SERVICE_FILENAME}
 }
 
 action="$1"
